e17a2bffd84c7885ea4e533d070462f59373153f
Victor <92888336+IbanezRubioVictor@users.noreply.github.com>
2024-11-22 20:03:29 +0100

    second commit

---
+++ src/utils/es/navigation.ts
@@ -0,0 +1,44 @@
+
+const navBarLinks = [
+  { name: "Home", url: "/fr" },
+  { name: "Productos", url: "/fr/products" },
+  { name: "Servicios", url: "/fr/services" },
+  
+ //{ name: "Blog", url: "/fr/blog" },
+
+  { name: "Contacto", url: "/fr/contact" },
+];
+
+const footerLinks = [
+  {
+    section: "Ecosistema",
+    links: [
+      { name: "Documentación", url: "/fr/welcome-to-docs/" },
+      { name: "Nuestros Equipamentos", url: "/fr/products" },
+      { name: "Servicios de Construcción", url: "/fr/services" },
+    ],
+  },
+  {
+    section: "Sociedad",
+    links: [
+      { name: "A proposito de nosotros", url: "#" },
+      { name: "Blog", url: "/fr/blog" },
+      { name: "Carreras", url: "#" },
+      { name: "Clientes", url: "#" },
+    ],
+  },
+];
+
+const socialLinks = {
+  facebook: "#",
+  x: "#",
+  github: "https://github.com/mearashadowfax/ScrewFast",
+  google: "#",
+  slack: "#",
+};
+
+export default {
+  navBarLinks,
+  footerLinks,
+  socialLinks,
+};
---
+++ src/utils/ui.ts
@@ -0,0 +1,5 @@
+export const languages = {
+    en: "English",
+    fr: "Español"
+    
+};
