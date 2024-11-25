709a9236a167c47945bc462ad2a68f9180610d3a
Victor <92888336+IbanezRubioVictor@users.noreply.github.com>
2024-11-25 19:59:03 +0100

    change configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,8 @@ import starlight from "@astrojs/starlight";
 // https://astro.build/config
 export default defineConfig({
   // https://docs.astro.build/en/guides/images/#authorizing-remote-images
-  site: "https://screwfast.uk",
+  site: "https://github.com/VictorRubioIba",
+  base:"MotorMecanic",
   image: {
     domains: ["images.unsplash.com"],
   },
