a1ec222bf8391c4ff38b7e4dee89f05bb5e6bc7d
Victor <92888336+IbanezRubioVictor@users.noreply.github.com>
2024-11-26 23:10:18 +0100

    repair fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import starlight from "@astrojs/starlight";
 // https://astro.build/config
 export default defineConfig({
   // https://docs.astro.build/en/guides/images/#authorizing-remote-images
-  site: "https://VictorRubioIba.github.io",
+  site: "https://github.com/VictorRubioIba",
   base:"",
   image: {
     domains: ["images.unsplash.com"],
