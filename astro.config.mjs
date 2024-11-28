5507bbca77e213f3b5dc3e980f4319003941d38a
Victor <92888336+IbanezRubioVictor@users.noreply.github.com>
2024-11-28 08:35:35 +0100

    change direction on config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,16 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import compressor from "astro-compressor";
 import starlight from "@astrojs/starlight";
-const isBuild = import.meta.env.PROD;
+
+const SERVER_PORT = 4321;
+
+const LOCALHOST_URL = "http://localhost: ${SERVER_PORT}";
 
 const LIVE_URL ="https://victorrubioiba.github.io"
+
+const SCRIPT = process.env.npm_lifecycle_script || "";
+const isBuild = SCRIPT.includes("astro build");
+
 let BASE_URL = LIVE_URL
 
 if(isBuild) {
@@ -14,9 +21,11 @@ if(isBuild) {
 
 // https://astro.build/config
 export default defineConfig({
-  // https://docs.astro.build/en/guides/images/#authorizing-remote-images
+  server: {
+    port: SERVER_PORT,
+  },
   site: BASE_URL,
-  base: "MotorMecanic",
+  base: "",
   output: 'static',
   image: {
     domains: ["images.unsplash.com"],
