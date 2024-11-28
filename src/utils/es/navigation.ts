93e9deb1f7aff1b11fb39c6a310564294b008a5c
Victor <92888336+IbanezRubioVictor@users.noreply.github.com>
2024-11-28 15:40:24 +0100

    change direction on config

--- src/utils/es/navigation.ts
+++ src/utils/es/navigation.ts
@@ -1,12 +1,12 @@
 
 const navBarLinks = [
-  { name: "Home", url: "/MotorMecanic/es" },
-  { name: "Productos", url: "/MotorMecanic/es/products" },
-  { name: "Servicios", url: "/MotorMecanic/es/services" },
+  { name: "Home", url: "/es/MotorMecanic" },
+  { name: "Productos", url: "/es/MotorMecanic/products" },
+  { name: "Servicios", url: "/es/MotorMecanic/services" },
   
  //{ name: "Blog", url: "/fr/blog" },
 
-  { name: "Contacto", url: "/MotorMecanic/es/contact" },
+  { name: "Contacto", url: "/es/MotorMecanic/contact" },
 ];
 
 const footerLinks = [
